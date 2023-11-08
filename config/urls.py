60738d5b6a377d11638ae9a62d197e6de22afc93
Varda666 <oksana.aeshina666@gmail.ru>
2023-11-08 22:05:38 +0300

    first commit lesson 24.1

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('lms_service.urls', 'lms_service'), namespace='lms_service')),
+    # path('users/', include(('users.urls', 'users'), namespace='users'))
+    ]
+
